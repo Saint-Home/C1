50e78980c84decd813ea99eb88ee17421fed7d3d
MSI <moonsungin0124@gmail.com>
2022-01-19 02:38:19 +0900

    Add (C#) Winform Base Source

---
+++ KVLS2_C1/View/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace KVLS2_C1
+{
+    public partial class MainForm : Form
+    {
+        #region Main Form 시작
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+    }
+}
